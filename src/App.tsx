8910b046d560a2f7f761cfb863da832d26dad107
Taras-B <taras5475@gmail.com>
2021-01-15 14:03:41 +0200

    Create form component, test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
+import { ExpenseForm } from './components/NewExpenseForm'
 
 function App() {
   return (
     <div className="App">
-      test
+      <ExpenseForm></ExpenseForm>
     </div>
   );
 }
---
+++ src/components/NewExpenseForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ExpenseForm = () => {
+    return (
+        <div>
+            form
+        </div>
+    )
+}
